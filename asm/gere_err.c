844a46ae3251338db1b0cfd2545bd848a7406ec4
limone_m <limone_m@epitech.eu>
2014-03-18 20:13:22 +0100

    SOCLE

---
+++ asm/gere_err.c
@@ -0,0 +1,30 @@
+/*
+** gere_err.c for gere_err in /home/limone_m/rendu/CPE_2014_corewar/asm
+** 
+** Made by Maxime Limone
+** Login   <limone_m@epitech.net>
+** 
+** Started on  Tue Mar 18 17:06:04 2014 Maxime Limone
+** Last update Tue Mar 18 20:05:47 2014 Maxime Limone
+*/
+
+#include <stdlib.h>
+#include "asm.h"
+
+int		check_file_name(char *file)
+{
+  int	i;
+
+  i = my_strlen(file) - 2;
+  if (file[i] != 's' && file[i - 1] != '.')
+    my_exit_err("incompatible file (type file is [.s])");
+  
+}
+
+void		my_exit_err(char *message_err)
+{
+  my_putstr("\e[1;31mError: ");
+  my_putstr(message_err);
+  my_putstr("\e[0m\n");
+  exit(3);
+}
--- asm/main.c
+++ asm/main.c
@@ -5,10 +5,22 @@
 ** Login   <limone_m@epitech.net>
 ** 
 ** Started on  Tue Mar 18 16:19:28 2014 Maxime Limone
-** Last update Tue Mar 18 16:19:49 2014 Maxime Limone
+** Last update Tue Mar 18 20:08:54 2014 Maxime Limone
 */
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int		main(int argc, char **argv)
 {
+  int		fd;
+
+  if (argc < 2)
+    my_exit_err("Usage: ./asm/asm [file_name].s");
+  check_file_name(argv[1]);
+  if ((fd = open(argv[1], O_RDONLY)) == -1)
+    my_exit_err("file not found");
   return (0);
 }
