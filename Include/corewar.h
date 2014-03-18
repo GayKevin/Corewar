c736fcc0b50cf456ea211d496d159e99e8e89ba3
limone_m <limone_m@epitech.eu>
2014-03-18 16:30:07 +0100

    SOCLE

---
+++ Include/corewar.h
@@ -0,0 +1,14 @@
+/*
+** corewar.h for corewar in /home/limone_m/rendu/CPE_2014_corewar/Include
+** 
+** Made by Maxime Limone
+** Login   <limone_m@epitech.net>
+** 
+** Started on  Tue Mar 18 16:29:04 2014 Maxime Limone
+** Last update Tue Mar 18 16:29:48 2014 Maxime Limone
+*/
+
+#ifndef COREWAR_H_
+# define COREWAR_H_
+
+#endif /*COREWAR_H_*/
